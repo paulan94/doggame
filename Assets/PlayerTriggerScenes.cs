3415e11f699a540b8a3f9b626aa4ea02b60a996b
paulan94 <paulan94@gmail.com>
2021-04-11 02:20:30 -0700

    add new peepad game scene, and set cameras

---
+++ Assets/PlayerTriggerScenes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerTriggerScenes : MonoBehaviour
+{
+
+    public GameObject peePadTrigger;
+    public GameObject parkGameTrigger;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject == peePadTrigger){
+            Debug.Log("triggering pee pad game");
+            SceneManager.LoadScene("PeePadGame");
+        }
+        if (other.gameObject == parkGameTrigger){
+            Debug.Log("triggering park game");
+            SceneManager.LoadScene("PeePadGame"); //chgange this to park game.
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Animator anim;
+    // Rigidbody rb;
+    // CharacterController controller;
+
+    // public float speed = 5;
+    // public float gravity = -5;
+
+    // float yVelocity = 0;
+
+
+	private Animator animator;
+	public float xRotation = 10f;
+	public float mouseSensitivity = 100f;
+	
+	public CharacterController charController;
+	public float speed = 12f;
+	public float gravity = -10f;
+	public float jumpHeight = 2f;
+
+	public Transform groundCheck;
+	public float groundDistance = 0.4f;
+	public LayerMask groundMask;
+	public Vector3 velocity;
+	public bool isGrounded;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // ControllPlayer();
+        HandleMovement();
+		RotatePlayer();
+    }
+
+    void HandleMovement()
+	{
+
+		float h;
+		float v;
+		bool jumpPressed = false;
+
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+		jumpPressed = Input.GetButtonDown("Jump");
+
+
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = -1.4f;
+		}
+
+		Vector3 move = transform.right * h + transform.forward * v;
+
+
+		charController.Move(move * speed * Time.deltaTime);
+	
+
+		if (jumpPressed && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -1.4f * gravity);
+			animator.SetTrigger("jump");
+		}
+		else
+		{
+			animator.ResetTrigger("jump");
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+
+		if (velocity != Vector3.zero){
+			animator.SetInteger("Walk", 1);
+		}
+		else{
+			animator.SetInteger("Walk", 0);
+		}
+
+		charController.Move(velocity * Time.deltaTime);
+
+	}
+
+	void RotatePlayer()
+	{
+		//clamp to just rotate on y only camera and head only
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		Vector3 rotPlayer = transform.rotation.eulerAngles;
+		rotPlayer.y += mouseX;
+
+		transform.localRotation = Quaternion.Euler(0f, rotPlayer.y, 0f);
+		transform.Rotate(Vector3.up * mouseX);
+
+	}
+}
