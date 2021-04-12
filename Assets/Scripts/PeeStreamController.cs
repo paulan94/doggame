453261e59daf950b931d5297cab72f7111dbc979
paulan94 <paulan94@gmail.com>
2021-04-12 16:28:58 -0700

    update again

--- Assets/Scripts/PeeStreamController.cs
+++ Assets/Scripts/PeeStreamController.cs
@@ -36,34 +36,18 @@ public class PeeStreamController : MonoBehaviour
     //todo: clamp the stream so it doesnt go out of bounds? or maybe keep it like this so if it goes out of bounds, player loses points or something.
     void MovePeeStream(){
 
-        // float h = horizontalSpeed * Input.GetAxis("Mouse X");
-        // float v = verticalSpeed * Input.GetAxis("Mouse Y");
-
-        // transform.Rotate(-v, h, 0);
-
         Vector3 upAxis = new Vector3(-1,-1,-1);
         Vector3 mouseScreenPosition = Input.mousePosition;
-        //set mouses z to your targets
+        //set mouse y,z to your targets
         mouseScreenPosition.z = transform.position.z;
         mouseScreenPosition.y = -mouseScreenPosition.y;
         Vector3 mouseWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
         transform.LookAt(mouseWorldSpace, upAxis);
+
         //zero out all rotations except the axis I want
         transform.eulerAngles = new Vector3(-transform.eulerAngles.x,-transform.eulerAngles.y,-transform.eulerAngles.z);
 
 
-        // rotX += Input.GetAxis("Mouse X")* horizontalSpeed;
-        // rotY += Input.GetAxis ("Mouse Y")* verticalSpeed;
- 
-        // rotY = Mathf.Clamp(rotY, clampMin, clampMax);    
-        // rotX = Mathf.Clamp(rotX, clampMin, clampMax);       
- 
-        // //Camera rotation only allowed if game us not paused
-        // // Camera.transform.rotation = Quaternion.Euler(-rotY, 0f, 0f);
-        // transform.rotation = Quaternion.Euler(rotY, rotX, 0f);
-
-        // Cursor.visible = false;
-        // Cursor.lockState = CursorLockMode.Locked;
     }
 
 }
